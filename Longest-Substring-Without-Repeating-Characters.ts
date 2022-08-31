c49b1de24614b7aa5e806e654fe4ef78c8782aff
fahimSiam <siamfahim02@gmail.com>
2022-09-01 00:34:40 +0600

    test

---
+++ Longest-Substring-Without-Repeating-Characters.ts
@@ -0,0 +1,12 @@
+function lengthOfLongestSubstring(s: string): number {
+  const lenghtOfSubString = 0;
+  let subString = "";
+  for (var char of s) {
+    if (subString.toLowerCase().indexOf("stream") === -1
+     && char.toLowerCase().indexOf("stream") === -1) {
+        var a = "..."
+    }
+  }
+  return lenghtOfSubString;
+}
+lengthOfLongestSubstring("aaccsd");
